d844632b3b07b4c10560c2288b58740929a93f3f
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-12-23 19:03:55 +0700

    Add error definitions for user authentication validation

---
+++ authentications/internal/commons/exceptions/errors.go
@@ -0,0 +1,12 @@
+package exceptions
+
+import "errors"
+
+var (
+	ErrEmailInvalid = errors.New("email is invalid")
+	ErrEmailRequired = errors.New("email is required")
+	ErrPasswordRequired = errors.New("password is required")
+	ErrEmailAlreadyUsed = errors.New("email is already used")
+	ErrPasswordInvalidLength = errors.New("password length must be at least 8 characters")
+	ErrFullNameRequired = errors.New("full name is required")
+)
