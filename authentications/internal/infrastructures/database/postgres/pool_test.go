e60fba3a4603a749ef7a25b78a3b0783d235f6dc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-12-25 17:00:07 +0700

    Add tests for PostgreSQL connection handling

---
+++ authentications/internal/infrastructures/database/postgres/pool_test.go
@@ -0,0 +1,45 @@
+package postgres
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ardwiinoo/micro-music/authentications/config"
+)
+
+func init() {
+	if err := config.LoadConfig("../../../../.env"); err != nil {
+		panic(err)
+	}
+}
+
+func TestConnectionPostgres(t *testing.T) {
+	originalConfig := config.Cfg
+
+	t.Run("Success Connect Postgres", func(t *testing.T) {
+		db, err := ConnectPostgres()
+
+		require.Nil(t, err)
+		require.NotNil(t, db)
+
+		if db != nil {
+			db.Close()
+		}
+	})
+
+	t.Run("Failed Connect Postgres", func(t *testing.T) {
+		config.Cfg.DB.Host = "invalid_host"
+		config.Cfg.DB.Port = 9999
+		config.Cfg.DB.User = "invalid_user"
+		config.Cfg.DB.Password = "invalid_password"
+		config.Cfg.DB.DBName = "invalid_db"
+
+		db, err := ConnectPostgres()
+
+		require.NotNil(t, err)
+		require.Nil(t, db)
+
+		config.Cfg = originalConfig
+	})
+}
