6b1f95bccda0c7743900ecab56648d0f94b8c392
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-12-26 18:29:58 +0700

    Add LoginUserUseCase to container for user authentication

--- authentications/internal/infrastructures/container.go
+++ authentications/internal/infrastructures/container.go
@@ -3,17 +3,20 @@ package infrastructures
 import (
 	"github.com/jmoiron/sqlx"
 
+	"github.com/ardwiinoo/micro-music/authentications/config"
 	usecase "github.com/ardwiinoo/micro-music/authentications/internal/applications/use_case"
 	"github.com/ardwiinoo/micro-music/authentications/internal/domains/users"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/database/postgres"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/repository"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/security"
+
 )
 
 type Container struct {
 	DB *sqlx.DB
 	UserRepository   users.UserRepository
 	RegisterUserUseCase usecase.RegisterUserUseCase
+	LoginUserUseCase usecase.LoginUserUseCase
 }
 
 func NewContainer() (container *Container, err error) {
@@ -25,15 +28,18 @@ func NewContainer() (container *Container, err error) {
 	}
 
 	passwordHash := security.NewPasswordHash()
+	pasetoManager := security.NewPasetoTokenManager(config.Cfg.App.AppSecret.AppPrivateKey, config.Cfg.App.AppSecret.AppPublicKey)
 
 	userRepo := repository.NewUserRepositoryPostgres(db)
 
 	registerUserUseCase := usecase.NewRegisterUserUseCase(userRepo, passwordHash)
+	loginUserUseCase := usecase.NewloginUserUseCase(userRepo, passwordHash, pasetoManager)
 
 	return &Container{
 		DB: db,
 		UserRepository: userRepo,
 		RegisterUserUseCase: registerUserUseCase,
+		LoginUserUseCase: loginUserUseCase,
 	}, nil
 }
 
