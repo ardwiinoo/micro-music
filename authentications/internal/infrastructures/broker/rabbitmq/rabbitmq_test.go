ad45f4b06e04a590452a82c4872e0138997e1c44
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-05 08:45:23 +0700

    Refactor RabbitMQ tests to use event publishing and improve naming conventions

--- authentications/internal/infrastructures/broker/rabbitmq/rabbitmq_test.go
+++ authentications/internal/infrastructures/broker/rabbitmq/rabbitmq_test.go
@@ -32,27 +32,36 @@ func TestRabbitMQ(t *testing.T) {
 		require.Nil(t, rabbit)
 	})
 
-	t.Run("RabbitMQ Publish Message Successfully", func(t *testing.T) {
+	t.Run("RabbitMQ Publish Event Successfully", func(t *testing.T) {
 		rabbit, err := rabbitmq.NewRabbitMQ(config.Cfg.Rabbit.ConnString)
-		
+
 		require.Nil(t, err)
 		defer rabbit.Close()
 
-		queueName := "test_queue"
-		message := "Hello, RabbitMQ!"
+		queueName := "test_events"
+		eventType := "test_event"
+		payload := map[string]string{
+			"message": "Hello, RabbitMQ!",
+		}
 
-		err = rabbit.PublishMessage(queueName, message)
+		err = rabbit.PublishEvent(queueName, eventType, payload)
 		require.Nil(t, err)
 	})
 
-	t.Run("RabbitMQ Publish Message Fail", func(t *testing.T) {
+	t.Run("RabbitMQ Publish Event Fail", func(t *testing.T) {
 		rabbit, err := rabbitmq.NewRabbitMQ(config.Cfg.Rabbit.ConnString)
 		require.Nil(t, err)
 		defer rabbit.Close()
 
 		rabbit.Close()
 
-		err = rabbit.PublishMessage("test_queue", "This will fail")
+		queueName := "test_events"
+		eventType := "test_event"
+		payload := map[string]string{
+			"message": "This will fail",
+		}
+
+		err = rabbit.PublishEvent(queueName, eventType, payload)
 		require.NotNil(t, err)
 	})
 }
