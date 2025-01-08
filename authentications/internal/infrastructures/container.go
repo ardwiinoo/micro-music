fedb99350dca8e25f24a2e397e1dbd8efe6a5c93
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-08 21:23:52 +0700

    Integrate RabbitMQ into the authentication container for event handling

--- authentications/internal/infrastructures/container.go
+++ authentications/internal/infrastructures/container.go
@@ -6,10 +6,10 @@ import (
 	"github.com/ardwiinoo/micro-music/authentications/config"
 	usecase "github.com/ardwiinoo/micro-music/authentications/internal/applications/use_case"
 	"github.com/ardwiinoo/micro-music/authentications/internal/domains/users"
+	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/broker/rabbitmq"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/database/postgres"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/repository"
 	"github.com/ardwiinoo/micro-music/authentications/internal/infrastructures/security"
-
 )
 
 type Container struct {
@@ -17,26 +17,36 @@ type Container struct {
 	UserRepository   users.UserRepository
 	RegisterUserUseCase usecase.RegisterUserUseCase
 	LoginUserUseCase usecase.LoginUserUseCase
+	RabbitMQ *rabbitmq.RabbitMQ
 }
 
 func NewContainer() (container *Container, err error) {
 	
 	db, err := postgres.ConnectPostgres()
-
 	if err != nil {
 		return nil, err
 	}
 
+	rabbit, err := rabbitmq.NewRabbitMQ(config.Cfg.Rabbit.ConnString)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// Security
 	passwordHash := security.NewPasswordHash()
 	pasetoManager := security.NewPasetoTokenManager(config.Cfg.App.AppSecret.AppPrivateKey, config.Cfg.App.AppSecret.AppPublicKey)
 
+	// Repository
 	userRepo := repository.NewUserRepositoryPostgres(db)
 
-	registerUserUseCase := usecase.NewRegisterUserUseCase(userRepo, passwordHash)
+	// UseCase
+	registerUserUseCase := usecase.NewRegisterUserUseCase(userRepo, passwordHash, rabbit)
 	loginUserUseCase := usecase.NewloginUserUseCase(userRepo, passwordHash, pasetoManager)
 
 	return &Container{
 		DB: db,
+		RabbitMQ: rabbit,
 		UserRepository: userRepo,
 		RegisterUserUseCase: registerUserUseCase,
 		LoginUserUseCase: loginUserUseCase,
@@ -46,5 +56,9 @@ func NewContainer() (container *Container, err error) {
 func (c *Container) Close() {
 	if c.DB != nil {
 		c.DB.Close()
+	}
+	
+	if c.RabbitMQ != nil {
+		c.RabbitMQ.Close()
 	}
 }
