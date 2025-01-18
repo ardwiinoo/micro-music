f8a7d0e98ffb9bb361a99e0d6560f6f144d8ad67
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-18 15:41:16 +0700

    Implement song routes and handlers in base.go

--- musics/internal/interfaces/songs/base.go
+++ musics/internal/interfaces/songs/base.go
@@ -1,1 +1,19 @@
 package songs
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/ardwiinoo/micro-music/musics/internal/infrastructures"
+	"github.com/ardwiinoo/micro-music/musics/internal/infrastructures/http/middlewares"
+)
+
+func Init(router fiber.Router, container infrastructures.Container) {
+	
+	handler := NewSongHandler(container)
+	songRouter := router.Group("/songs")
+
+	{
+		songRouter.Get("/", handler.getListSongHandler)
+		songRouter.Post("/", middlewares.AuthFilter(container.TokenManager), handler.addSongHandler)
+	}
+}
