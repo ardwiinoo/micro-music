28a71c10e3790147c27d715d71e9dd415575df87
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-21 10:26:56 +0700

    Add Redis cache integration to the container

--- musics/internal/infrastructures/container.go
+++ musics/internal/infrastructures/container.go
@@ -4,9 +4,11 @@ import (
 	"github.com/jmoiron/sqlx"
 
 	"github.com/ardwiinoo/micro-music/musics/config"
+	appCache "github.com/ardwiinoo/micro-music/musics/internal/applications/cache"
 	appSecurity "github.com/ardwiinoo/micro-music/musics/internal/applications/security"
 	usecase "github.com/ardwiinoo/micro-music/musics/internal/applications/use_case"
 	"github.com/ardwiinoo/micro-music/musics/internal/domains/songs"
+	infraCache "github.com/ardwiinoo/micro-music/musics/internal/infrastructures/cache"
 	"github.com/ardwiinoo/micro-music/musics/internal/infrastructures/database/postgres"
 	"github.com/ardwiinoo/micro-music/musics/internal/infrastructures/repository"
 	infraSecurity "github.com/ardwiinoo/micro-music/musics/internal/infrastructures/security"
@@ -14,6 +16,7 @@ import (
 
 type Container struct {
 	DB *sqlx.DB
+	redis appCache.CacheManager
 	TokenManager appSecurity.TokenManager
 	SongRepository songs.SongRepository
 	AddSongUseCase usecase.AddSongUseCase
@@ -29,6 +32,9 @@ func NewContainer() (container *Container, err error) {
 		return nil, err
 	}
 
+	// Cache
+	redis := infraCache.NewRedisCache(config.Cfg.Cache.Host, config.Cfg.Cache.Password, config.Cfg.Cache.DB)
+
 	// Security
 	pasetoManager := infraSecurity.NewPasetoTokenManager(config.Cfg.App.AppSecret.AppPublicKey)
 
@@ -42,6 +48,7 @@ func NewContainer() (container *Container, err error) {
 
 	return &Container{
 		DB: db,
+		redis: redis,
 		TokenManager: pasetoManager,
 		SongRepository: songRepository,
 		AddSongUseCase: addSongUseCase,
