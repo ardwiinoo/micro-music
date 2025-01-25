ee61da16bf4835584a7ccaca49d659b1a010c514
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-25 15:57:59 +0700

    Add constants for queue types and event types in authentication module

---
+++ authentications/internal/commons/constants/constant.go
@@ -0,0 +1,13 @@
+package constants
+
+var QueueTypes = struct {
+	EmailQueue string
+}{
+	EmailQueue: "email_events",
+}
+
+var EventType = struct {
+	UserRegistered string
+}{
+	UserRegistered: "user_registered",
+}
