c7f19ddf482e82f6e01421cf6737c25367c9cf36
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-29 10:03:38 +0700

    Add error mappings for user-related issues in AddPlaylist use case

--- playlists/internal/commons/exceptions/translator/errors.go
+++ playlists/internal/commons/exceptions/translator/errors.go
@@ -4,5 +4,9 @@ import "github.com/ardwiinoo/micro-music/playlists/internal/commons/exceptions"
 
 var DomainErrorMapping = map[string]exceptions.AppError{
 	"ADD_PLAYLIST.NOT_CONTAIN_NEEDED_PROPERTY": exceptions.InvariantError("Add playlist not contain needed property"),
+	"ADD_PLAYLIST_USE_CASE.PUBLIC_ID_NOT_FOUND": exceptions.InvariantError("Public id not found"),
+	"ADD_PLAYLIST_USE_CASE.USER_NOT_FOUND": exceptions.NotFoundError("User not found"),
+	"ADD_PLAYLIST_USE_CASE.USER_NOT_AUTHORIZED": exceptions.ForbiddenError("User not authorized"),
 	"DETAIL_PLAYLIST.NOT_CONTAIN_NEEDED_PROPERTY": exceptions.InvariantError("Detail playlist not contain needed property"),
+
 }
