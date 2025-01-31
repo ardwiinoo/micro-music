2d5c01d32808b21b6843b1f65fa4ec4185f4a9d4
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-01-31 09:46:12 +0700

    Add delete, validate owner, & add playlist song abstract

--- playlists/internal/domains/playlists/playlist_repository.go
+++ playlists/internal/domains/playlists/playlist_repository.go
@@ -1,11 +1,15 @@
 package playlists
 
 import (
+	"context"
 	"github.com/google/uuid"
 
 	"github.com/ardwiinoo/micro-music/playlists/internal/domains/playlists/entities"
 )
 
 type PlaylistRepository interface {
-	AddPlaylist(playlist *entities.AddPlaylist, userID int) (playlistId uuid.UUID, err error)
+	AddPlaylist(ctx context.Context, playlist *entities.AddPlaylist, userID int) (playlistId uuid.UUID, err error)
+	DeletePlaylist(ctx context.Context, playlistID uuid.UUID) (err error)
+	ValidatePlaylistOwner(ctx context.Context, playlistID uuid.UUID, userID int) (err error)
+	AddPlaylistSong(ctx context.Context, playlistID uuid.UUID, songID uuid.UUID) (err error)
 }
