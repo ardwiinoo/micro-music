d15d2720a2d7729871dce470bc39ef8ef38a5531
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-02 11:55:54 +0700

    Adjust owner from uuid to string

--- playlists/internal/domains/playlists/entities/detail_playlist.go
+++ playlists/internal/domains/playlists/entities/detail_playlist.go
@@ -7,15 +7,15 @@ import (
 )
 
 type DetailPlaylist struct {
-	ID uuid.UUID `json:"id" db:"id"`
-	Name string `json:"name" db:"name"`
-	OwnerID uuid.UUID `json:"owner_id" db:"owner_id"`
-	CreatedAt string `json:"created_at" db:"created_at"`
-	UpdatedAt string `json:"updated_at" db:"updated_at"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Owner     string    `json:"owner" db:"owner"`
+	CreatedAt string    `json:"created_at" db:"created_at"`
+	UpdatedAt string    `json:"updated_at" db:"updated_at"`
 }
 
 func (a *DetailPlaylist) Validate() (err error) {
-	if a.ID == uuid.Nil || a.Name == "" || a.OwnerID == uuid.Nil || a.CreatedAt == "" || a.UpdatedAt == "" {
+	if a.ID == uuid.Nil || a.Name == "" || a.Owner == "" || a.CreatedAt == "" || a.UpdatedAt == "" {
 		return errors.New("DETAIL_PLAYLIST.NOT_CONTAIN_NEEDED_PROPERTY")
 	}
 
