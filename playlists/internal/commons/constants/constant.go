a8853ead49ca93b4f1c5980a1e423aedce90f4bb
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-03 09:24:12 +0700

    Add contants for queue & event rabbitMQ

--- playlists/internal/commons/constants/constant.go
+++ playlists/internal/commons/constants/constant.go
@@ -6,6 +6,20 @@ var (
 	PublicIDContextKey ContextKey = "public_id"
 )
 
+var QueueTypes = struct {
+	EmailQueue string
+}{
+	EmailQueue: "email_events",
+}
+
+var EventType = struct {
+	UserRegistered string
+	ExportPlaylist string
+}{
+	UserRegistered: "user_registered",
+	ExportPlaylist: "export_playlist",
+}
+
 const (
 	UserRoleID  int = 1
 	AdminRoleID int = 2
