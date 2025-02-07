90d1c14766d2d8061826e826b12b22bf046cfd33
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-07 08:52:26 +0700

    Add error handler middleware for centralized error management

---
+++ users/internal/infrastructures/http/middlewares/error_handler.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"github.com/ardwiinoo/micro-music/users/internal/commons/exceptions"
+	"github.com/ardwiinoo/micro-music/users/internal/commons/exceptions/translator"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+	var appErr exceptions.AppError
+
+	if e, ok := err.(exceptions.AppError); ok {
+		appErr = e
+	} else {
+		if translatedErr, exists := translator.DomainErrorMapping[err.Error()]; exists {
+			appErr = translatedErr
+		} else {
+			appErr = exceptions.InternalServerError(err.Error())
+		}
+	}
+
+	status := "error"
+	if appErr.HttpCode >= 400 && appErr.HttpCode < 500 {
+		status = "fail"
+	}
+
+	return ctx.Status(appErr.HttpCode).JSON(fiber.Map{
+		"status":    status,
+		"http_code": appErr.HttpCode,
+		"message":   appErr.Message,
+	})
+}
