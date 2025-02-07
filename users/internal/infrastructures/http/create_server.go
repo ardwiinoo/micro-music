943b1db48f08f00877726c07c3bd17b954925acc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-07 08:52:22 +0700

    Add HTTP server creation with middleware configuration

---
+++ users/internal/infrastructures/http/create_server.go
@@ -0,0 +1,28 @@
+package http
+
+import (
+	"github.com/ardwiinoo/micro-music/users/config"
+	"github.com/ardwiinoo/micro-music/users/internal/infrastructures"
+	"github.com/ardwiinoo/micro-music/users/internal/infrastructures/http/middlewares"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func CreateServer(container *infrastructures.Container) *fiber.App {
+	router := fiber.New(fiber.Config{
+		Prefork:      true,
+		AppName:      config.Cfg.App.Name,
+		ErrorHandler: middlewares.ErrorHandler,
+	})
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+
+	router.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	return router
+}
