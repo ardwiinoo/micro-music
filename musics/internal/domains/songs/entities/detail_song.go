21133a6b6fd64424724709ffea6cf5421b26191b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-02-10 19:03:51 +0700

    Add Url field to DetailSong entity and update validation

--- musics/internal/domains/songs/entities/detail_song.go
+++ musics/internal/domains/songs/entities/detail_song.go
@@ -7,14 +7,15 @@ import (
 )
 
 type DetailSong struct {
-	ID     uuid.UUID    `json:"id" db:"id"`
-	Title  string `json:"title" db:"title"`
-	Year   int    `json:"year" db:"year"`
-	Artist string `json:"artist" db:"artist"`
+	ID     uuid.UUID `json:"id" db:"id"`
+	Title  string    `json:"title" db:"title"`
+	Year   int       `json:"year" db:"year"`
+	Artist string    `json:"artist" db:"artist"`
+	Url    string    `json:"url" db:"url"`
 }
 
 func (d *DetailSong) Validate() (err error) {
-	if d.ID == uuid.Nil || d.Title == "" || d.Year == 0 || d.Artist == "" {
+	if d.ID == uuid.Nil || d.Title == "" || d.Year == 0 || d.Artist == "" || d.Url == "" {
 		return errors.New("DETAIL_SONG.NOT_CONTAIN_NEEDED_PROPERTY")
 	}
 
