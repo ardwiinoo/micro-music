9bf933d66caa4f24df32182532ef0724c0f364ad
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-03-03 08:34:57 +0700

    Add user repository methods for user management operations

--- users/internal/domains/users/user_repository.go
+++ users/internal/domains/users/user_repository.go
@@ -1,4 +1,14 @@
 package users
 
+import (
+	"context"
+	"github.com/ardwiinoo/micro-music/users/internal/domains/users/entities"
+	"github.com/google/uuid"
+)
+
 type UserRepository interface {
+	GetListUser(ctx context.Context) (users []entities.DetailUser, err error)
+	AddUser(ctx context.Context, payload entities.AddUser) (publicId uuid.UUID, err error)
+	VerifyAvailableEmail(ctx context.Context, email string) (err error)
+	GetDetailUserByPublicId(ctx context.Context, publicId string) (userDetail entities.DetailUser, err error)
 }
